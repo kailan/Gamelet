a9ba0f2f125f6b5c7746f618e67a72aaeccd8122
Kailan Blanks <kailan@kmp.pw>
2017-05-14 21:34:35 +0100

    Don't add players to a team when they join

--- Core/src/main/kotlin/pw/kmp/gamelet/modules/teams/TeamModule.kt
+++ Core/src/main/kotlin/pw/kmp/gamelet/modules/teams/TeamModule.kt
@@ -3,7 +3,6 @@ package pw.kmp.gamelet.modules.teams
 import pw.kmp.gamelet.Playerlet
 import pw.kmp.gamelet.match.Match
 import pw.kmp.gamelet.modules.GameletModule
-import pw.kmp.gamelet.modules.players.PlayerJoinMatchEvent
 import pw.kmp.gamelet.modules.players.PlayerLeaveMatchEvent
 import pw.kmp.gamelet.modules.players.PlayerModule
 
@@ -16,9 +15,6 @@ open class TeamModule(val match: Match, val players: PlayerModule) {
     val teams = mutableSetOf<Team>()
 
     init {
-        match.subscribe<PlayerJoinMatchEvent>(-1) {
-            teams.filter { !it.isFull() }.sortedBy { it.players.size }.firstOrNull()?.addPlayer(player) // TODO: join commands
-        }
         match.subscribe<PlayerLeaveMatchEvent> {
             get(player)?.removePlayer(player)
         }
